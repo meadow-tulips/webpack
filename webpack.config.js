9ccff6b770a3bd5e845ded045c085a26676a92e6
Avan Sharma <avan@zopsmart.com>
2021-04-04 23:30:44 +0530

    Added webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+var path = require('path');
+
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist")
+    }
+}
