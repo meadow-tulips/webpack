6e43b732b0e607a72f878093c2b1e800b85f583d
Avan Sharma <avan@zopsmart.com>
2021-04-05 08:25:10 +0530

    Added scss loader

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { run } from './app/app';
 import AlertService from './app/alert-service';
 import ComponentService from './app/component-service';
-import './main.css';
+import './main.scss';
 
 const alertInstance = new AlertService();
 const componentInstance = new ComponentService();
