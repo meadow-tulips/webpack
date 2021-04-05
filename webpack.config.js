35e57ef4df528819dab524eb66ecb151ad9849a1
Avan Sharma <avan@zopsmart.com>
2021-04-05 08:54:21 +0530

    Added html webpack Plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,21 @@
 var path = require('path');
+var webpackPlugin = require('html-webpack-plugin');
 
 
 module.exports = {
     mode: "development",
     entry: "./src/index.js",
     output: {
-        filename: "main.js",
+        filename: "main.[contenthash].js",
         path: path.resolve(__dirname, "dist")
     },
-
+    plugins:[new webpackPlugin({
+        template: "./src/template.html",
+    })],
     module : {
         rules: [{
             test: /\.scss$/,
             use: ['style-loader', 'css-loader', 'sass-loader']
         }]
-    }
+    },
 }
