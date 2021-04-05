177637b1ab82c0d2a3bc29cacef438c6651af724
Avan Sharma <avan@zopsmart.com>
2021-04-05 08:12:17 +0530

    Added first css loader

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { run } from './app/app';
 import AlertService from './app/alert-service';
 import ComponentService from './app/component-service';
+import './main.css';
 
 const alertInstance = new AlertService();
 const componentInstance = new ComponentService();
